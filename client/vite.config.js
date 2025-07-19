caa954fa1d6d13b12b4438e08c401f9aecc2ac98
mishima1992 <101380699+mishima1992@users.noreply.github.com>
2025-07-19 11:38:17 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Kita akan jalankan client di port 3000
+    port: 3000,
     host: true,
     proxy: {
       '/api': {
@@ -13,6 +12,13 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
+    hmr: {
+      clientPort: 443
+    },
+    allowedHosts: [
+      'mainserver.streamyard.online',
+      'auth.streamyard.online',
+    ],
   }
 })
